e1021c54d4520f8e018a10a4554f5f058eb42fd5
Thanh An Vu <anvu.infosec@gmail.com>
2025-02-04 12:25:21 -0600

    First commit on 2/4/2025

---
+++ Bingo.java
@@ -0,0 +1,16 @@
+/**
+ * @author: Thanh An Vu
+ * @version: 1.0
+ * Date created: 2/4/2025
+ */
+
+
+import java.util.Scanner;
+public class Bingo {
+    private int target;
+    private int lines;
+    public static void main(String[] args) {
+        System.out.println("Assignment 2: BINGO!");
+        Scanner scnr = new Scanner("BingoCards.txt");
+    }
+}
